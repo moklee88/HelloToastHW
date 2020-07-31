e545a4fbc5b0e0ec62c43206094297139c961923
moklee88 <cuihaomok88@gmail.com>
2020-07-31 12:15:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloToastHW/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.helloToastHW;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    public static final String EXTRA_NUMBER
+            = "com.example.helloToastHW.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+    public void sayHello(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        String  num = mShowCount.getText().toString();
+        intent.putExtra(EXTRA_NUMBER, num);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
